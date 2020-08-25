da78dbdcd5f95088cd7ca9c58d9c31a66ea4f0fd
Jörg Brandeis <github@brandeis.de>
2020-08-25 14:56:53 +0200

    Update 04_Check_Installation_EN.sql
    Update for SAP HANA Cloud / Database Explorer

--- demo_data_model/04_Check_Installation_EN.sql
+++ demo_data_model/04_Check_Installation_EN.sql
@@ -1,20 +1,20 @@
 
-select count(*) as cnt, 'users' as table from users                --shoud be 30
+select count(*) as cnt, 'users' as tab from users                --shoud be 30
 UNION
-  select count(*) as cnt, 'projects' as table from  projects       --shoud be 5
+  select count(*) as cnt, 'projects' as tab from  projects       --shoud be 5
 UNION
-  select count(*) as cnt, 'tasks' as table from  tasks             --shoud be 1000
+  select count(*) as cnt, 'tasks' as tab from  tasks             --shoud be 1000
 UNION
-  select count(*) as cnt, 'status' as table from  status           --shoud be 6
+  select count(*) as cnt, 'status' as tab from  status           --shoud be 6
 UNION
-select count(*) as cnt, 'status_text' as table from  status_text   --should be 12
+select count(*) as cnt, 'status_text' as tab from  status_text   --should be 12
 UNION
-select count(*) as cnt, 'team_text' as table from  team_text       --should be 13
+select count(*) as cnt, 'team_text' as tab from  team_text       --should be 14
 UNION
-select count(*) as cnt, 'tasks_log' as table from  tasks_log       --should be empty
+select count(*) as cnt, 'tasks_log' as tab from  tasks_log       --should be empty
 UNION
-  select count(*) as cnt, 'dats_date' as table from  dats_date     --should be empty
+  select count(*) as cnt, 'dats_date' as tab from  dats_date     --should be empty
 UNION
-  select count(*) as cnt, 'colors' as table from  colors           --should be empty
+  select count(*) as cnt, 'colors' as tab from  colors           --should be empty
 UNION
-select count(*) as cnt, 'sizes' as table from  sizes;              --should be empty
+select count(*) as cnt, 'sizes' as tab from  sizes;              --should be empty
